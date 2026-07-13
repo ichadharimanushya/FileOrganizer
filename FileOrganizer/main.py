a45292ff54abda321c8130b772ed09891c30beab
ichadharimanushya <parthlohani2008@gmail.com>
2026-07-13 13:51:39 +0530

    Phrase 1

--- FileOrganizer/main.py
+++ FileOrganizer/main.py
@@ -1,2 +1,9 @@
-import pathlib  # for the path of the files and stuff
+from pathlib import Path  # for the path of the files and stuff
 import shutil   # moving and copying the files  
+
+WORKING_DIR = Path().home()/"Downloads" # from where the files will be abducted...yes abducted!
+DIRECTED_DIR = Path("D:/AllDocuments") # folders where the files will be sorted into
+
+downloaded_files =  list(WORKING_DIR.iterdir())
+organised_folders = list(DIRECTED_DIR.iterdir())
+

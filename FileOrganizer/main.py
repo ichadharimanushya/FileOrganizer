cb9e105b0a53a1f7a38fbac2f940a73b608f3b0e
ichadharimanushya <parthlohani2008@gmail.com>
2026-07-16 14:37:42 +0530

    Added watchdog and it's a mess

--- FileOrganizer/main.py
+++ FileOrganizer/main.py
@@ -2,7 +2,9 @@ from pathlib import Path  # for the path of the files and stuff
 import shutil   # moving and copying the files  
 import json     # for the love of the game
 import logging  # for recording every step in case of something something...
-import watchdog # keeps an eye for a new folder
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+import time
 
 SOURCE_DIR = Path().home()/"Downloads" # from where the files will be abducted...yes abducted!
 DESTINATION_DIR = Path("D:/AllDocuments") # location where the files will be sorted into different folders
@@ -14,6 +16,13 @@ logging.basicConfig(level = logging.INFO,
 
 with open(Path(__file__).parent/"config.json", 'r') as f:
     data = json.load(f)
+
+class FolderHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        if event.is_directory:
+            log(f"SKIPPED OVER THE FOLDER: {event.src_path}")
+            return
+        organise_file(Path(event.src_path), DESTINATION_DIR)
 
 def log(message):
     logging.info(message)
@@ -39,23 +48,37 @@ def get_folder_name(ext, configurations):
             return foldername
     return None
 
-def organise_file(source_path, destination_path):
-    for file in Path(source_path).iterdir():
-        if file.is_file():
-            folder = get_folder_name(file.suffix, data)
-            if folder == None:
-                log(f"UNKNOWN FILE FORMAT FOR: {file}")
-                continue
-            create_folder(destination_path/folder)
-            destination = unique_filename(file, destination_path/folder)
+def organise_file(file, destination_path):
+    folder = get_folder_name(file.suffix, data)
+    if folder is None:
+        log(f"UNKNOWN FILE FORMAT FOR: {file}")
+        return
+    create_folder(destination_path/folder)
+    destination = unique_filename(file, destination_path/folder)
+    while 1:
+        try:
             shutil.move(src = file, dst = destination)
             log(f"MOVED FILE: | FROM: {file} | TO: {destination}")
-        else:
-            log(f"SKIPPED OVER THE FOLDER: {file}")
+            break
+        except PermissionError as e:
+            time.sleep(5)
+
+def watchdog_observer():
+    event_handler = FolderHandler() # instance of handler
+    observer = Observer()           # instance of observer
+    observer.schedule(event_handler, path=str(SOURCE_DIR), recursive=False)
+    log(f"Monitoring started for: {SOURCE_DIR}")
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt: # ctrl + c
+        observer.stop()
+    observer.join()     # Wait until the background thread finishes cleaning up
 
 def main():
     log("PROGRAM STARTED")
-    organise_file(SOURCE_DIR, DESTINATION_DIR)
+    watchdog_observer()
     log("PROGRAM ENDED\n\n\n")
 
 if __name__ == "__main__":
